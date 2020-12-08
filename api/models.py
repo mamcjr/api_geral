67e8dac851b6b419b12f2958d0372f104d6d30f9
mamcj <juniorpmcn@gmail.com>
2020-12-08 00:30:20 -0300

    commit inicial

---
+++ api/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from .util import estados
+
+# Create your models here.
+class Pessoa(models.Model):
+    """
+    Model para controle de pessoas com dados pessoais
+    """
+
+    class Meta:
+        db_table = 'pessoa'
+
+    nome = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11)
+    email = models.CharField(max_length=100)
+    endereco = models.ForeignKey('Endereco', on_delete=models.CASCADE)
+    foto = models.FileField(blank=True, null=True)
+
+class Endereco(models.Model):
+    """
+    Model para Controle de Endereços
+    """
+
+    class Meta:
+        db_table = 'endereco'
+
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=8)
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cidade = models.IntegerField()
+    uf = models.CharField(max_length=2,choices=estados.STATE_CHOICES)
